9f040c3af36e73779647c8a2e6f68c32e92bcc06
Maurice Bailleu <M.Bailleu@web.de>
2020-10-19 16:54:21 +0100

    Added time flag

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,25 @@ use clap::{Arg, App};
 use std::thread;
 use std::time::Duration;
 
-fn reset(port: u8) {
+fn reset(port: u8, time: u64) {
     let led = LED::new(port);
     led.on();
-    thread::sleep(Duration::from_secs(5));
+    thread::sleep(Duration::from_secs(time));
     led.off();
 }
 
 fn main() {
     let matches = App::new("Reset program for servers in Edinburgh")
         .version("0.1.0")
-        .arg(Arg::with_name("port")
+        .arg(Arg::with_name("server")
             .required(true)
-            .help("Port to send reset"))
+            .help("Server to send reset"))
+        .arg(Arg::with_name("time")
+             .help("Time to send requests defaults to 5"))
         .get_matches();
 
-    let server = matches.value_of("port").unwrap_or("-1");
+    let server = matches.value_of("server").unwrap_or("-1");
+    let time : u64 = matches.value_of("time").unwrap_or("5").parse().unwrap();
     println!("The server you chose is: {}", server);
 
     let port : i64 = match server {
@@ -33,7 +36,7 @@ fn main() {
     if port != -1 && port < 256 {
         println!("Port is {}", port);
         println!("Reset...");
-        reset(port as u8);
+        reset(port as u8, time);
         println!("Done.");
         return;
     }
